1a17a38e691dd3cee77d4312fd703094da0f0676
Thomas Linssen <t.linssen@live.nl>
2024-06-04 16:19:03 +0200

    Added 'base' directory, might be deleted/changed later

--- pse_backend/pse_backend/urls.py
+++ pse_backend/pse_backend/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("satellite_app/", include("satellite_app.urls")),
+    path("admin/", admin.site.urls),
 ]
