25a141129d9972facda91451fa5f8bd960b6fa4c
Sasha van der Linden <sasha.van.der.linden@student.uva.nl>
2024-06-06 14:38:02 +0200

    Made Satellite catalog number primary key for satellite model

--- pse_backend/satellite_app/views.py
+++ pse_backend/satellite_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-def index(request):
+def index(request: HttpRequest):
+    print(request.GET)
     return HttpResponse("Some placeholder text.", content_type="text/plain")
