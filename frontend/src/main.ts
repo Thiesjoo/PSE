18c28e0fff9ace4f9821d3a94b0e2b8bff03f1eb
Thies <git@thies.dev>
2024-06-10 15:49:12 +0200

    Added abstractions for simulation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,20 @@
 import './style.scss';
 
-import EarthWithSatellites from './Earth';
+// import EarthWithSatellites from './Earth';
+import {ThreeSimulation} from './Sim';
+import { Satellite } from './Satellite';
+
 
 window.addEventListener('DOMContentLoaded', () => {
-    new EarthWithSatellites();
+    const sim = new ThreeSimulation();
+
+    const sat = new Satellite();
+    sat.fromTLE(`ISS (ZARYA)
+1 25544U 98067A   21197.51736111  .00000913  00000-0  23866-4 0  9996
+2 25544  51.6443  89.0000 0002866  93.0000  17.0000 15.48900079286168`);
+
+    sim.addSatellite(sat);
+    sim.setTimeSpeed(1);
+
+    // new EarthWithSatellites();
 });
