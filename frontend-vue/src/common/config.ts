f8922aedf01136377a8d8ae99c1653a4a7350105
Thies <git@thies.dev>
2024-06-11 14:46:57 +0200

    Always use prod api

--- frontend-vue/src/common/config.ts
+++ frontend-vue/src/common/config.ts
@@ -1,4 +1,9 @@
-export const OVERRIDE_API = false;
+export const ALWAYS_USE_PROD = true;
 
+const API_DEV_URL = 'http://localhost:8000/';
+const API_PROD_URL = 'https://pseapi.thies.dev/';
 
-export const API_URL = (import.meta.env.MODE === 'development' || !OVERRIDE_API) ? 'http://localhost:8000/' : 'https://pseapi.thies.dev/'
+export const API_URL = (ALWAYS_USE_PROD ? API_PROD_URL : (
+    import.meta.env.MODE === 'development' ? API_DEV_URL : API_PROD_URL
+))
+
