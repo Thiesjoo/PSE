0ec1c69894826ea14c199124967d5fc33ae88c65
Wike <w.j.duivenvoorden@outlook.com>
2024-06-12 16:12:47 +0200

    copy pasta button

--- frontend-vue/src/views/Simulation.vue
+++ frontend-vue/src/views/Simulation.vue
@@ -76,6 +76,13 @@ watch(e, (Value) => {
 })
 
 props.simulation.setTimeSpeed(50)
+
+// ********* SATELLITE SPEED *********
+let speed = ref(1)
+watch(speed, (newSpeed) => {
+  props.simulation.setTimeSpeed(newSpeed)
+})
+
 </script>
 
 <template>
@@ -140,6 +147,7 @@ props.simulation.setTimeSpeed(50)
       </div>
     </div>
   </div>
+  <button class="speed-button" @click="speed = speed * 2">Speed * 2. Current speed: {{ speed }}</button>
 </template>
 
 <style scoped lang="scss">
@@ -156,6 +164,7 @@ h3 {
   text-align: center;
   font-weight: bold;
 }
+
 .left-info-block {
   position: absolute;
   top: 0px;
@@ -187,6 +196,16 @@ h3 {
   padding: 20px;
   background-color: rgba(195, 0, 255, 0.36);
   border-radius: 200px;
+  cursor: pointer;
+  color: white;
+}
+
+.speed-button {
+  appearance: none;
+  bottom: 50px;
+  padding: 20px;
+  background-color: rgba(195, 0, 255, 0.36);
+  border-radius: 2px;
   cursor: pointer;
   color: white;
 }
