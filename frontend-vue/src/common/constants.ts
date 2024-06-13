cc5fb9f71cecc6a45c503117be4b60ac60ca05b3
Thies <git@thies.dev>
2024-06-13 13:29:07 +0200

    Always propagate all sats every frame, gives less FPS but higher perceived performance

--- frontend-vue/src/common/constants.ts
+++ frontend-vue/src/common/constants.ts
@@ -13,7 +13,7 @@ export const MIN_CAMERA_DISTANCE = 0
 // Het hoger zetten zorgt er wel voor dat er meer satellieten gerenderd worden, en dus de performance omlaag gaat.
 export const MAX_SATS_TO_RENDER = 12_000;
 
-export const MAX_PROPAGATES_PER_FRAME = 1000;
+export const MAX_PROPAGATES_PER_FRAME = MAX_SATS_TO_RENDER;
 if (MAX_PROPAGATES_PER_FRAME * 60 < MAX_SATS_TO_RENDER) {
   console.warn(
     'MAX_PROPAGATES_PER_FRAME * 60 < renderLimit, satellieten worden niet allemaal binnen 1 seconde geupdate. Kan leiden tot onverwacht gedrag.'
