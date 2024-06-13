ea0da33cd62887bb14e297124377b283260f025a
Thies <37243142+Thiesjoo@users.noreply.github.com>
2024-06-13 16:40:10 +0200

    Change camera limits and scale satellites (#47)

--- frontend-vue/src/common/constants.ts
+++ frontend-vue/src/common/constants.ts
@@ -5,7 +5,7 @@ export const SAT_COLOR = 'palegreen'
 export const SAT_COLOR_HOVER = '#FFD700'
 export const SAT_COLOR_SELECTED = '#FF4500'
 export const LINE_SIZE = 10000
-export const MAX_CAMERA_DISTANCE = 700
+export const MAX_CAMERA_DISTANCE = 5000
 export const MIN_CAMERA_DISTANCE = 150
 
 // TODO: Dit is het maximale aantal satellieten dat uberhaup gerenderd kan worden. Misschien hoger maken?
