1d9a4f4690d8f861dca638a581b7e41e709fae29
Thomas <thomasjvanlieshout@gmail.com>
2024-06-17 15:01:37 +0200

    add sentry tracking

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,12 +6,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), sentryVitePlugin({
+    org: "none-gty",
+    project: "javascript-vue"
+  })],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
