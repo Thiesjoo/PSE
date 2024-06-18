32a9b44607bcdcf6f7e12dc9717c6777a3dcc969
Thomas Linssen <t.linssen@live.nl>
2024-06-18 12:38:03 +0200

    Basic slider working in ui

--- frontend-vue/src/components/FilterBar.vue
+++ frontend-vue/src/components/FilterBar.vue
@@ -28,6 +28,56 @@ const value = ref([1960,2024]);
       <FilterItem v-for="filter in filters" :key="filter.name" v-model="filter.selected">
         {{ filter.name }} - {{ manager.count[filter.name] }}
       </FilterItem>
+      
+      <div class="launch-year-filter-block">
+        <label>Filter on launch years</label>
+        <vue-slider v-model="slider_values" 
+          :min="FIRST_LAUNCH_YEAR" 
+          :max="MOST_RECENT_LAUNCH_YEAR"
+          :min-range="1"
+          :enable-cross="false"
+          id="launch-year-slider"/>
+          
+        <input type="checkbox" id="include_without_launch_year" v-model="include_sats_without_launch_year">
+        <label for="include_without_launch_year">Include satellites without launch year</label>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.left-info-block {
+  position: absolute;
+  top: 0px;
+  left: 0;
+  width: 320px;
+  height: 100%;
+  background-color: #05050a7c;
+  color: white;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border: 2px solid rgba(255, 255, 255, 0.75);
+  border-radius: 12px;
+  padding: 50px;
+}
+
+.tmp {
+  display: flex;
+  flex-direction: column;
+}
+
+.launch-year-filter-block {
+  margin-top: 20px;
+}
+
+#include_without_launch_year {
+  margin-top: 5px;
+  margin-right: 5px;
+}
+
     </div>
     <vue-slider v-model="value" :min="1960" :max="2024" :min-range="1" :enable-cross="false" id="launch-year-slider" />
   </div>
