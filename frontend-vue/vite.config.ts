7bafd2a5e516d2b7995b676ee2f32451ac7c4807
Thomas <thomasjvanlieshout@gmail.com>
2024-06-20 15:53:09 +0200

    remove double code

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -8,9 +8,6 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueI18nPlugin({}), sentryVitePlugin({
-    org: 'satradar',
-    project: 'javascript-vue'
-  }), sentryVitePlugin({
     org: "satradar",
     project: "javascript-vue"
   })],
