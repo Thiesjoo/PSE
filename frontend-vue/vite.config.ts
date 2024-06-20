26af28fd4818162989eecfc6c4ffdcf840b66c58
Thomas <thomasjvanlieshout@gmail.com>
2024-06-20 15:44:29 +0200

    Update sentry

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -7,14 +7,13 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({}),
-    sentryVitePlugin({
-      org: 'satradar',
-      project: 'javascript-vue'
-    })
-  ],
+  plugins: [vue(), VueI18nPlugin({}), sentryVitePlugin({
+    org: 'satradar',
+    project: 'javascript-vue'
+  }), sentryVitePlugin({
+    org: "satradar",
+    project: "javascript-vue"
+  })],
 
   resolve: {
     alias: {
