fecd3cf04dadeba9c5b86732179acd5700ca3715
Gosia <m.k.gniadek@student.tudelft.nl>
2024-06-24 16:04:22 +0200

    formatter

--- frontend-vue/src/views/Simulation.vue
+++ frontend-vue/src/views/Simulation.vue
@@ -204,7 +204,13 @@ props.simulation.addEventListener('select', (satellite) => {
 
 <template>
   <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
   </head>
   <LeftInfoBlock :open="true" class="container">
     <br />
@@ -259,10 +265,12 @@ props.simulation.addEventListener('select', (satellite) => {
     </div>
     <br />
     <div class="button-box">
-      <button class="add-del-button" @click="add = 1" style="text-align: center"><i class="fa-solid fa-plus"></i>
+      <button class="add-del-button" @click="add = 1" style="text-align: center">
+        <i class="fa-solid fa-plus"></i>
         <!-- {{ t('Add satellite') }} -->
       </button>
-      <button class="add-del-button" @click="remove = 1" style="text-align: center"><i class="fa-regular fa-trash-can"></i>
+      <button class="add-del-button" @click="remove = 1" style="text-align: center">
+        <i class="fa-regular fa-trash-can"></i>
         <!-- {{ t('Delete satellites') }} -->
       </button>
     </div>
