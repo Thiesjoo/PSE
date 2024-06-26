7fa9047ae523dd47d7028a60ceadd7ded9ef16a7
Thies <git@thies.dev>
2024-06-26 12:58:25 +0200

    Reset to current time now correctly updates the orbit

--- frontend-vue/src/Time.ts
+++ frontend-vue/src/Time.ts
@@ -6,6 +6,8 @@ export class Time {
   public multiplier: Ref<number> = ref(1)
   private clock: THREE.Clock = new THREE.Clock()
 
+  private listeners = new Set<() => void>()
+
   get time() {
     return this.currentTime
   }
@@ -16,14 +18,30 @@ export class Time {
 
   setTime(time: Date) {
     this.currentTime = time
+
+    for (const listener of this.listeners) {
+      listener()
+    }
   }
 
   setSpeed(speed: number) {
     this.multiplier.value = speed
+
+    for (const listener of this.listeners) {
+      listener()
+    }
   }
 
   step() {
     const delta = this.clock.getDelta()
     this.currentTime = new Date(+this.currentTime + this.multiplier.value * 1000 * delta)
+  }
+
+  addEventListener(listener: () => void) {
+    this.listeners.add(listener)
+  }
+
+  removeEventListener(listener: () => void) {
+    this.listeners.delete(listener)
   }
 }
