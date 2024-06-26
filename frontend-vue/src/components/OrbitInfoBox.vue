34e5483721247ff18a70fbdcaf33801111ef7ef6
Thies <git@thies.dev>
2024-06-26 14:04:37 +0200

    Moved font-awesome icons to index.html

--- frontend-vue/src/components/OrbitInfoBox.vue
+++ frontend-vue/src/components/OrbitInfoBox.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-const { t, locale } = useI18n()
+const { t } = useI18n()
 
-const props = defineProps<{
+defineProps<{
   picked: string
 }>()
 </script>
 
 <template>
-  <head>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-  </head>
   <div>
     <!-- Information box HEIGHT -->
     <div class="orbit-info-box" v-show="['LEO', 'MEO', 'GEO'].includes(picked)">
