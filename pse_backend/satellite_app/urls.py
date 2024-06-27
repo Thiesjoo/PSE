44f07597814edb0c5a0cf1e39552e7e2258b7d56
Thomas Linssen <t.linssen@live.nl>
2024-06-27 14:02:02 +0200

    Removed unnecessary '/pull' endpoint

--- pse_backend/satellite_app/urls.py
+++ pse_backend/satellite_app/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("categories", views.categories, name="categories"),
     path("launch_years", views.launch_years, name="launch_years"),
     path("countries", views.countries, name="countries"),
-    path("pull", views.pull, name="pull"),
 ]
